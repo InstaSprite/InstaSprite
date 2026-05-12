281807c59048fb01af3f503607a5b2df47cb1ba2
phuocn0302 <phuocnguyen230302@gmail.com>
2026-05-13 01:05:13 +0700

    chore: bump version code

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -27,7 +27,7 @@ android {
         applicationId = "com.instasprite.app"
         minSdk = 26
         targetSdk = 36
-        versionCode = 1
+        versionCode = 2
         versionName = "0.7"
 
         buildConfigField("String", "BASE_URL", localProperties.getProperty("BASE_URL", "\"\""))
